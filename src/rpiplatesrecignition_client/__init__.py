802ec2b71a28ad61a2df289ba38075831ef2ea69
lukiwa <lukasz.iwan.99@gmail.com>
2021-03-30 12:37:00 +0200

    Moved from diffrent repo, TODO unit tests

--- src/rpiplatesrecignition_client/__init__.py
+++ src/rpiplatesrecignition_client/__init__.py
@@ -1,4 +1,6 @@
 import socketio
+from rpiplatesrecignition_client.libs.camera import camera
+
 
 def run(server, unique_id=''):
     sio = socketio.Client()
@@ -11,6 +13,11 @@ def run(server, unique_id=''):
     def open_gate():
         # open gate
         pass
+
+    @sio.on('take_photo')
+    def take_photo():
+        [token, img] = camera.take_photo()
+        
 
     sio.connect(server)
     sio.wait()
