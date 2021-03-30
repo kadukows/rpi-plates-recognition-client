4dbf9b00c5ff8a9aec93311573cda51e38a18ff4
lukiwa <lukasz.iwan.99@gmail.com>
2021-03-30 20:04:20 +0200

    Moved gate controller from diffrent repo, add simple test

--- src/rpiplatesrecignition_client/__init__.py
+++ src/rpiplatesrecignition_client/__init__.py
@@ -1,4 +1,6 @@
 import socketio
+from rpiplatesrecignition_client.libs.gate import gate
+
 
 def run(server, unique_id=''):
     sio = socketio.Client()
@@ -9,8 +11,15 @@ def run(server, unique_id=''):
 
     @sio.on('open_gate')
     def open_gate():
-        # open gate
-        pass
+        gate.open()
+        #todo, return statues eg. if gate is open or not
+
+    @sio.on('close_gate')
+    def close_gate():
+        gate.close()
+        #todo, return statues eg. if gate is open or not
+
+        
 
     sio.connect(server)
     sio.wait()
--- tests/test_rpiplatesrecognition_client.py
+++ tests/test_rpiplatesrecognition_client.py
@@ -1,5 +1,5 @@
 import rpiplatesrecignition_client
-
+from rpiplatesrecignition_client.libs.gate import gate
 
 def test_rpiplatesrecognition_client_will_call_login_upon_connecting(client_server):
     client, server = client_server
@@ -15,3 +15,13 @@ def test_rpiplatesrecognition_client_will_call_login_upon_connecting(client_serv
 
     rpiplatesrecignition_client.run('', unique_id)
     assert Recorder.called
+
+def test_gate_open_will_make_gate_busy(client_server):
+    assert gate.is_closed()
+    
+    client, server = client_server
+    unique_id = 'some_unique_id_on_rpi'
+    rpiplatesrecignition_client.run('', unique_id)
+    server.emit("open_gate")
+
+    assert gate.is_busy()
