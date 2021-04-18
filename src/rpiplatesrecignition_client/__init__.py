4e7fdb9d457431c1752d7449b7a632a5c29e20f1
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-04-18 16:34:53 +0200

    wip

--- src/rpiplatesrecignition_client/__init__.py
+++ src/rpiplatesrecignition_client/__init__.py
@@ -3,24 +3,32 @@ from .libs import logger_config
 from .libs import example_module
 
 def run(server, unique_id=''):
+    assert server.startswith('http:')
+
     sio = socketio.Client()
     logger_config.logger_config(sio)
     logger = logging.getLogger('rpiplatesrecognition_client.init')
 
-    @sio.on('open_gate')
-    def open_gate():
-        # open gate
-        logger.debug('received open gate message')
-        pass
-
-    @sio.on('run_example_module')
-    def run_example_module():
-        example_module.run()
-
-    @sio.event
+    @sio.event(namespace='/rpi')
     def connect():
-        print('login call')
-        sio.emit('login', data={'unique_id': unique_id})
+        res = sio.call(
+            'login_from_rpi',
+            data={'unique_id': unique_id},
+            namespace='/rpi')
+
+        print('response to login call: ', res)
+
+    @sio.on('message_from_server_to_rpi', namespace='/rpi')
+    def command(data):
+        if isinstance(data, str):
+            command_ = data
+
+            if command_ == 'run_example_module':
+                example_module.run()
+            elif command_ == 'open_gate':
+                logger.debug('open_gate was issued')
+            else:
+                logger.warning(f'Unknown command: {command_}')
 
     sio.connect(server)
     sio.wait()
